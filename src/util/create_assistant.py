8c61e794dfe6e9f9b4a9eaf03e7c9260ea5d6591
Cameron McConnell <cameron@angry-dogman.com>
2024-06-18 14:39:40 -0500

    Updated and tested create_assistant script locally and successfully

--- src/util/create_assistant.py
+++ src/util/create_assistant.py
@@ -7,14 +7,16 @@ client = OpenAI(
     organization=os.environ.get("CODECRAFT_CLI_ORGANIZATION_ID")
 )
 
+instructions = "You will only generate Python code using libraries like NumPy and Pandas \
+for data processing prompts. Only answer questions relevant to the csv file you are \
+supplied in the thread. If the prompt is not a data processing operation, return an \
+empty string."
+
 assistant = client.beta.assistants.create(
     name="codecraft_cli_assistant",
-    instructions="You will only generate Python code using libraries like NumPy and Pandas \
-        for data processing prompts. Only answer questions relevant to the csv file you are \
-        supplied in the thread. If the prompt is not a data processing operation, return an \
-        empty string.",
+    instructions=instructions,
     tools=[{"type": "code_interpreter"}],
-    model="gpt3.5-turbo"
+    model="gpt-3.5-turbo"
 )
 
-os.environ["CODECRAFT_CLI_ASSISTANT_ID"] = assistant.id
+print(f"CODECRAFT_CLI_ASSISTANT_ID={assistant.id}")
